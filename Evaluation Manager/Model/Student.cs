453e719d5f0d133c6a39e871fe713c742860b963
Tea Radić <tradic21@foi.hr>
2023-05-20 18:06:00 +0200

    Implementacija hijerarhije

---
+++ Evaluation Manager/Model/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation_Manager.Model
+{
+    internal class Student
+    {
+        public int Grade { get; set; }
+    }
+}
